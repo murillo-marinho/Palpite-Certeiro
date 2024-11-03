c1732a64102095573d01111643b12875b8384847
murillo-marinho <muriloxp552@gmail.com>
2024-11-03 08:24:42 -0300

    phases edition

--- js/fases.js
+++ js/fases.js
@@ -23,3 +23,55 @@ function navigateToPhase(phase) {
 window.onload = function() {
     document.body.style.opacity = '1';
 };
+
+
+const modal = document.getElementById("myModal");
+const exitButton = document.getElementById("exitButton");
+const confirmExit = document.getElementById("confirmExit");
+const cancelExit = document.getElementById("cancelExit");
+const closeButton = document.getElementsByClassName("close")[0];
+
+
+exitButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+confirmExit.onclick = function() {
+
+    window.location.href = "home.html"; 
+}
+
+cancelExit.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+function fadeOutAndRedirect(url) {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
+    overlay.style.pointerEvents = 'auto';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+    overlay.style.opacity = '0';
+
+    setTimeout(() => {
+        overlay.style.transition = 'opacity 2s';
+        overlay.style.opacity = '1';
+
+        setTimeout(() => {
+            window.location.href = url;
+        }, 2000);
+    }, 0);
+}
---
+++ js/reino.poseidon.js
@@ -0,0 +1,59 @@
+function redirect() {
+    document.body.style.transition = 'opacity 2s';
+    document.body.style.opacity = '0';
+
+    setTimeout(() => {
+        window.location.href = 'Fases.html';
+    }, 2000);
+}
+
+window.onload = function() {
+    document.body.style.opacity = '1';
+    startTyping();
+};
+
+const dialogue = [
+    "Eu sou Poseidon, o deus dos mares.","",
+    "Meu reino, repleto de vida e abundância, está ameaçado por forças sombrias que desejam destruir o que construímos.","",
+    "Preciso da sua ajuda para a realização de um desafio crucial que determinará o destino do meu reino."
+];
+
+let textPosition = 0;
+let charIndex = 0;
+const typingSpeed = 50;
+const dialogText = document.getElementById('dialogText');
+const continueButton = document.getElementById('continueButton');
+
+function startTyping() {
+    setTimeout(typeText, 2000);
+}
+
+function typeText() {
+    if (textPosition < dialogue.length) {
+        if (charIndex < dialogue[textPosition].length) {
+            dialogText.innerHTML += dialogue[textPosition].charAt(charIndex);
+            charIndex++;
+            setTimeout(typeText, typingSpeed);
+        } else {
+            dialogText.innerHTML += "<br>";
+            textPosition++;
+            charIndex = 0; 
+            
+           
+            if (textPosition < dialogue.length) {
+                setTimeout(typeText, typingSpeed); 
+            } else {
+                continueButton.style.display = 'block'; 
+            }
+        }
+    }
+}
+
+function redirect() {
+    document.body.style.transition = 'opacity 2s';
+    document.body.style.opacity = '0';
+
+    setTimeout(() => {
+        window.location.href = 'Fases.html';
+    }, 2000);
+}
